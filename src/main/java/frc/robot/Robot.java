fa555d570ff661dda63910400b0997a2faab769c
Abby Speckhals <speckhalsa@region18.org>
2022-01-24 19:41:17 -0500

    Created Catapult Class+ compressor start code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,13 +4,15 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,9 +21,12 @@ import edu.wpi.first.networktables.NetworkTableInstance;
  * project.
  */
 public class Robot extends TimedRobot {
+  private static final PneumaticsModuleType CTREPCM = null;
+
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private Compressor airCompressor;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -32,11 +37,15 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    airCompressor = new Compressor(CTREPCM);
+    airCompressor.enableDigital();
+   }
+    
 
     // SmartDashboard.putString("Is Finished", "robot init");
     // SmartDashboard.putBoolean("is interrupted", false);
 
-  }
+  
 
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
