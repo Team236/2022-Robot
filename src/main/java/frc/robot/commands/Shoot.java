f4026683771de5540cab25a36a7a2c156a933d9c
speckhalsa <speckhalsa@region18.org>
2022-02-05 17:53:23 -0500

    temporarily commented out shooter code

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -1,64 +1,64 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+// package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
-import frc.robot.Constants.ShooterConstants;
-import frc.robot.subsystems.Shooter;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.Constants;
+// import frc.robot.Constants.ShooterConstants;
+// import frc.robot.subsystems.Shooter;
 
-public class Shoot extends CommandBase {
+// public class Shoot extends CommandBase {
 
-  private Shooter shooterSub;
-  private double botSpeed, topSpeed;
+//   private Shooter shooterSub;
+//   private double botSpeed, topSpeed;
 
-  /** Creates a new Shoot. */
-  public Shoot(Shooter shooterSub, double botSpeed, double topSpeed) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.shooterSub = shooterSub;
-    addRequirements(this.shooterSub);
+//   /** Creates a new Shoot. */
+//   public Shoot(Shooter shooterSub, double botSpeed, double topSpeed) {
+//     // Use addRequirements() here to declare subsystem dependencies.
+//     this.shooterSub = shooterSub;
+//     addRequirements(this.shooterSub);
 
-    this.botSpeed = botSpeed;
-    this.topSpeed = topSpeed;
-  }
+//     this.botSpeed = botSpeed;
+//     this.topSpeed = topSpeed;
+//   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    shooterSub.resetEncoders();
+//   // Called when the command is initially scheduled.
+//   @Override
+//   public void initialize() {
+//     shooterSub.resetEncoders();
 
-    shooterSub.setP(Constants.ShooterConstants.kPShoot);
-    shooterSub.setI(ShooterConstants.kIShoot);
-    shooterSub.setD(ShooterConstants.kDShoot);
-    shooterSub.setFF(ShooterConstants.kFFShoot);
-    shooterSub.setOutputRange();
+//     shooterSub.setP(Constants.ShooterConstants.kPShoot);
+//     shooterSub.setI(ShooterConstants.kIShoot);
+//     shooterSub.setD(ShooterConstants.kDShoot);
+//     shooterSub.setFF(ShooterConstants.kFFShoot);
+//     shooterSub.setOutputRange();
 
-  }
+//   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    shooterSub.setBotSetPoint(botSpeed);
-    shooterSub.setTopSetPoint(topSpeed);
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {
+//     shooterSub.setBotSetPoint(botSpeed);
+//     shooterSub.setTopSetPoint(topSpeed);
 
-    SmartDashboard.putNumber("bot shoot setpoint", botSpeed);
-    SmartDashboard.putNumber("top shoot setpoint", topSpeed);
-    SmartDashboard.putNumber("bot actual speed", shooterSub.getBotVelocity());
-    SmartDashboard.putNumber("top actual speed", shooterSub.getTopVelocity());
-  }
+//     SmartDashboard.putNumber("bot shoot setpoint", botSpeed);
+//     SmartDashboard.putNumber("top shoot setpoint", topSpeed);
+//     SmartDashboard.putNumber("bot actual speed", shooterSub.getBotVelocity());
+//     SmartDashboard.putNumber("top actual speed", shooterSub.getTopVelocity());
+//   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    shooterSub.stop();
-  }
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {
+//     shooterSub.stop();
+//   }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
-}
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
+// }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,112 +1,112 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+// package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.RelativeEncoder;
+// import com.revrobotics.SparkMaxPIDController;
+// import com.revrobotics.CANSparkMax.ControlType;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Shooter extends SubsystemBase {
+// public class Shooter extends SubsystemBase {
 
-  private CANSparkMax bottomRoller, topRoller;
-  private SparkMaxPIDController botPidController, topPidController;
-  private RelativeEncoder botEncoder, topEncoder;
+//   private CANSparkMax bottomRoller, topRoller;
+//   private SparkMaxPIDController botPidController, topPidController;
+//   private RelativeEncoder botEncoder, topEncoder;
 
-  /** Creates a new Shooter. */
-  public Shooter() {
+//   /** Creates a new Shooter. */
+//   public Shooter() {
 
-    bottomRoller = new CANSparkMax(39, MotorType.kBrushless);
-    topRoller = new CANSparkMax(7, MotorType.kBrushless);
+//     bottomRoller = new CANSparkMax(39, MotorType.kBrushless);
+//     topRoller = new CANSparkMax(7, MotorType.kBrushless);
 
-    bottomRoller.restoreFactoryDefaults();
-    topRoller.restoreFactoryDefaults();
+//     bottomRoller.restoreFactoryDefaults();
+//     topRoller.restoreFactoryDefaults();
 
-    botPidController = bottomRoller.getPIDController();
-    topPidController = topRoller.getPIDController();
+//     botPidController = bottomRoller.getPIDController();
+//     topPidController = topRoller.getPIDController();
 
-    botEncoder = bottomRoller.getEncoder();
-    topEncoder = topRoller.getEncoder();
+//     botEncoder = bottomRoller.getEncoder();
+//     topEncoder = topRoller.getEncoder();
 
-  }
+//   }
 
-  public void setBotSetPoint(double botSpeed) {
-    botPidController.setReference(botSpeed, ControlType.kVelocity);
-  }
+//   public void setBotSetPoint(double botSpeed) {
+//     botPidController.setReference(botSpeed, ControlType.kVelocity);
+//   }
 
-  public void setTopSetPoint(double topSpeed) {
-    topPidController.setReference(topSpeed, ControlType.kVelocity);
-  }
+//   public void setTopSetPoint(double topSpeed) {
+//     topPidController.setReference(topSpeed, ControlType.kVelocity);
+//   }
 
-  public void setP(double kP) {
-    botPidController.setP(kP);
-    topPidController.setP(kP);
-  }
+//   public void setP(double kP) {
+//     botPidController.setP(kP);
+//     topPidController.setP(kP);
+//   }
 
-  public void setI(double kI) {
-    botPidController.setI(kI);
-    topPidController.setI(kI);
-  }
+//   public void setI(double kI) {
+//     botPidController.setI(kI);
+//     topPidController.setI(kI);
+//   }
 
-  public void setD(double kD) {
-    botPidController.setD(kD);
-    topPidController.setD(kD);
-  }
+//   public void setD(double kD) {
+//     botPidController.setD(kD);
+//     topPidController.setD(kD);
+//   }
 
-  public void setFF(double kF) {
-    botPidController.setFF(kF);
-    topPidController.setFF(kF);
-  }
+//   public void setFF(double kF) {
+//     botPidController.setFF(kF);
+//     topPidController.setFF(kF);
+//   }
 
-  public void setOutputRange() {
-    topPidController.setOutputRange(0.0, 5000.0);
-    botPidController.setOutputRange(0.0, 5000.0);
-  }
+//   public void setOutputRange() {
+//     topPidController.setOutputRange(0.0, 5000.0);
+//     botPidController.setOutputRange(0.0, 5000.0);
+//   }
 
-  public void resetEncoders() {
-    botEncoder.setPosition(0);
-    topEncoder.setPosition(0);
-  }
+//   public void resetEncoders() {
+//     botEncoder.setPosition(0);
+//     topEncoder.setPosition(0);
+//   }
 
-  public double getBotEncoder() {
-    return botEncoder.getPosition();
-  }
+//   public double getBotEncoder() {
+//     return botEncoder.getPosition();
+//   }
 
-  public double getTopEncoder() {
-    return topEncoder.getPosition();
-  }
+//   public double getTopEncoder() {
+//     return topEncoder.getPosition();
+//   }
 
-  public double getBotVelocity() {
-    return topEncoder.getVelocity();
-  }
+//   public double getBotVelocity() {
+//     return topEncoder.getVelocity();
+//   }
 
-  public double getTopVelocity() {
-    return botEncoder.getVelocity();
-  }
+//   public double getTopVelocity() {
+//     return botEncoder.getVelocity();
+//   }
 
-  public void setBotSpeedRaw(double botSpeed) {
-    bottomRoller.set(botSpeed);
-  }
+//   public void setBotSpeedRaw(double botSpeed) {
+//     bottomRoller.set(botSpeed);
+//   }
 
-  public void setTopSpeedRaw(double botSpeed) {
-    topRoller.set(botSpeed);
-  }
+//   public void setTopSpeedRaw(double botSpeed) {
+//     topRoller.set(botSpeed);
+//   }
 
-  public void stop() {
-    setBotSpeedRaw(0);
-    setTopSpeedRaw(0);
-  }
+//   public void stop() {
+//     setBotSpeedRaw(0);
+//     setTopSpeedRaw(0);
+//   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-    SmartDashboard.putNumber("bot shoot velocity", getBotVelocity());
-    SmartDashboard.putNumber("top shoot velocity", getTopVelocity());
-  }
-}
+//   @Override
+//   public void periodic() {
+//     // This method will be called once per scheduler run
+//     SmartDashboard.putNumber("bot shoot velocity", getBotVelocity());
+//     SmartDashboard.putNumber("top shoot velocity", getTopVelocity());
+//   }
+// }
