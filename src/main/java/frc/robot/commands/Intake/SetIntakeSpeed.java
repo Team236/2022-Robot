69ed1fa13ecfcf3026fc57784ef576573e9ac24d
speckhalsa <speckhalsa@region18.org>
2022-02-10 13:52:36 -0500

    moved these commands to subfolders

--- src/main/java/frc/robot/commands/Intake/SetIntakeSpeed.java
+++ src/main/java/frc/robot/commands/Intake/SetIntakeSpeed.java
@@ -2,8 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.Intake;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
 
@@ -25,12 +26,24 @@ public class SetIntakeSpeed extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    intake.resetCounter();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     intake.setSpeed(speed);
+
+    SmartDashboard.putNumber("intake counter", intake.getBallCount());
+    
+    if (intake.getBallCount() == 1) {
+      intake.setSpeed(0);
+    } else if (intake.getBallCount() == 2) {
+      intake.setSpeed(0);
+    } else if (intake.getBallCount() == 0) {
+      intake.setSpeed(speed);
+    }
+
   }
 
   // Called once the command ends or is interrupted.
---
+++ src/main/java/frc/robot/commands/Shooter/Shoot.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Shooter;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.subsystems.Shooter;
+
+public class Shoot extends CommandBase {
+
+  private Shooter shooterSub;
+  private double botSpeed, topSpeed;
+
+  /** Creates a new Shoot. */
+  public Shoot(Shooter shooterSub, double botSpeed, double topSpeed) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.shooterSub = shooterSub;
+    addRequirements(this.shooterSub);
+
+    this.botSpeed = botSpeed;
+    this.topSpeed = topSpeed;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+    shooterSub.restoreFactoryDefaults();
+    shooterSub.resetEncoders();
+
+    shooterSub.setP(ShooterConstants.kPBot, ShooterConstants.kPTop);
+    shooterSub.setI(ShooterConstants.kIBot, ShooterConstants.kITop);
+    shooterSub.setD(ShooterConstants.kDBot, ShooterConstants.kDTop);
+    shooterSub.setFF(ShooterConstants.kFFBot, ShooterConstants.kFFTop);
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    shooterSub.setOutputRange();
+
+    shooterSub.setBotSetPoint(botSpeed);
+    shooterSub.setTopSetPoint(topSpeed);
+
+    // shooterSub.setMotorSpeeds(0.3, 0.3);
+    SmartDashboard.putNumber("bot shoot setpoint", botSpeed);
+    SmartDashboard.putNumber("top shoot setpoint", topSpeed);
+    SmartDashboard.putNumber("bot actual speed", shooterSub.getBotVelocity());
+    SmartDashboard.putNumber("top actual speed", shooterSub.getTopVelocity());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shooterSub.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
