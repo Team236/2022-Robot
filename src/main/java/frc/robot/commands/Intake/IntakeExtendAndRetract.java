bf8e8f8fd23772760dc9c7344cd1a83d3297ac52
speckhalsa <speckhalsa@region18.org>
2022-02-12 12:14:41 -0500

    intake toggle button logic not working

--- src/main/java/frc/robot/commands/Intake/IntakeExtendAndRetract.java
+++ src/main/java/frc/robot/commands/Intake/IntakeExtendAndRetract.java
@@ -1,47 +1,53 @@
 package frc.robot.commands.Intake;
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
 
-// import edu.wpi.first.wpilibj2.command.CommandBase;
-// import frc.robot.subsystems.Intake;
+public class IntakeExtendAndRetract extends CommandBase {
 
-// public class IntakeExtendAndRetract extends CommandBase {
-
-//   private Intake intake;
+  private Intake intake;
+  private boolean isExtended;
    
-//   /** Creates a new IntakeExtendAndRetract. */
-//   public IntakeExtendAndRetract() {
+  /** Creates a new IntakeExtendAndRetract. */
+  public IntakeExtendAndRetract(Intake intake) {
 
-//     this.intake = intake;
-//     // Use addRequirements() here to declare subsystem dependencies.
-//     addRequirements(intake);
-//   }
+    this.intake = intake;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake);
+  }
 
-//   // Called when the command is initially scheduled.
-//   @Override
-//   public void initialize() {
-    
-//     if (intake.isExtended()) {
-//       intake.retract();
-//     } else if (!intake.isExtended()) {
-//       intake.extend();
-//     }
-//   }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
 
-//   // Called every time the scheduler runs while the command is scheduled.
-//   @Override
-//   public void execute() {}
+    System.out.println("beginning if else");
+    if (intake.isExtended()) {
+        System.out.println("is extended");
+      intake.retract();    
+    } else if (!intake.isExtended()) {
+        System.out.println("is retracted");
 
-//   // Called once the command ends or is interrupted.
-//   @Override
-//   public void end(boolean interrupted) {}
+      intake.extend();
+    }
+   
+  }
 
-//   // Returns true when the command should end.
-//   @Override
-//   public boolean isFinished() {
-//     return false;
-//   }
-// }
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+ 
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
