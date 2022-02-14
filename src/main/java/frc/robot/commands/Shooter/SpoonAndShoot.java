3c8c96a05a19fb6e860228b6fda3311c6e3d6e11
speckhalsa <speckhalsa@region18.org>
2022-02-13 20:46:21 -0500

    initial spoon code

---
+++ src/main/java/frc/robot/commands/Shooter/SpoonAndShoot.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Shooter;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.commands.Spoon.SpoonExtend;
+import frc.robot.commands.Spoon.SpoonRetract;
+import frc.robot.subsystems.LoadingSpoon;
+import frc.robot.subsystems.Shooter;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class SpoonAndShoot extends SequentialCommandGroup {
+  /** Creates a new SpoonAndShoot. */
+  public SpoonAndShoot(LoadingSpoon loadingSpoon, Shooter shooter) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+
+    super(sequence(
+      new Shoot(shooter, ShooterConstants.HIGH_HUB_LARGE, ShooterConstants.HIGH_HUB_SMALL),
+      new WaitCommand(0.1),
+      new SpoonExtend(loadingSpoon),
+      new SpoonRetract(loadingSpoon)
+    ));
+  }
+}
---
+++ src/main/java/frc/robot/commands/Spoon/SpoonRetract.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Spoon;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.LoadingSpoon;
+
+public class SpoonRetract extends CommandBase {
+
+  private LoadingSpoon retractSpoon;
+
+  /** Creates a new SolenoidForward. */
+  public SpoonRetract(LoadingSpoon retractSpoon) {
+   this.retractSpoon = retractSpoon;
+    // Use addRequirements() here to declare subsystem dependencies.
+   addRequirements(this.retractSpoon);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    SmartDashboard.putString("spoon retract execute", "true");
+    retractSpoon.reverse();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
