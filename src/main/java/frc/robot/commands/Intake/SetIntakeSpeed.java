4dc48545b82196032058a9d9dd1a3823a3afddf5
speckhalsa <speckhalsa@region18.org>
2022-02-18 07:55:28 -0500

    simplified this command (intake w/out count)

--- src/main/java/frc/robot/commands/Intake/SetIntakeSpeed.java
+++ src/main/java/frc/robot/commands/Intake/SetIntakeSpeed.java
@@ -4,7 +4,6 @@
 
 package frc.robot.commands.Intake;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
 
@@ -12,38 +11,23 @@ public class SetIntakeSpeed extends CommandBase {
 
   private Intake intake;
   private double speed;
-  
+
   /** Creates a new SetIntakeSpeed. */
   public SetIntakeSpeed(Intake intake, double speed) {
-
-    this.intake = intake;
-    this.speed = speed;
-
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(this.intake);
+    this.speed = speed;
+    this.intake = intake;
+    addRequirements(intake);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    intake.resetCounter();
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     intake.setSpeed(speed);
-
-    SmartDashboard.putNumber("intake counter", intake.getBallCount());
-    
-    if (intake.getBallCount() == 1) {
-      intake.setSpeed(0);
-    } else if (intake.getBallCount() == 2) {
-      intake.setSpeed(0);
-    } else if (intake.getBallCount() == 0) {
-      intake.setSpeed(speed);
-    }
-
   }
 
   // Called once the command ends or is interrupted.
