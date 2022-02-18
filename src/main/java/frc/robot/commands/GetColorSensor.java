e18db220db201750d7b1bfd63e1ae95e655165c1
speckhalsa <speckhalsa@region18.org>
2022-02-18 07:51:03 -0500

    moved color sensor stuff to intake

--- src/main/java/frc/robot/commands/GetColorSensor.java
+++ src/main/java/frc/robot/commands/GetColorSensor.java
@@ -1,48 +1,50 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+// package frc.robot.commands;
 
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.ColorSensor;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.subsystems.ColorSensor;
 
-public class GetColorSensor extends CommandBase {
+// public class GetColorSensor extends CommandBase {
 
-  private ColorSensor colorSensor;
+//   private ColorSensor colorSensor;
 
-  /** Creates a new GetColorSensor. */
-  public GetColorSensor(ColorSensor colorSensor) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.colorSensor = colorSensor;
-    addRequirements(this.colorSensor);
-  }
+//   /** Creates a new GetColorSensor. */
+//   public GetColorSensor(ColorSensor colorSensor) {
+//     // Use addRequirements() here to declare subsystem dependencies.
+//     this.colorSensor = colorSensor;
+//     addRequirements(this.colorSensor);
+//   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    colorSensor.getAllianceColor();
+//   // Called when the command is initially scheduled.
+//   @Override
+//   public void initialize() {
+//     colorSensor.getAllianceColor();
 
-  }
+//   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    colorSensor.getColor();
-    colorSensor.publishAllianceColor();
-    colorSensor.getBlue();
-    colorSensor.getRed();
-    colorSensor.isBallMine();
-  }
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {
+//     colorSensor.getColor();
+//     colorSensor.publishAllianceColor();
+//     colorSensor.getBlue();
+//     colorSensor.getRed();
+//     colorSensor.isBallMine();
+//     colorSensor.getDistance();
+//   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
-}
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
+// }
