c02f85d8682cbb379fd5e348970317a62c49bf90
speckhalsa <speckhalsa@region18.org>
2022-03-16 20:54:31 -0400

    more shooter RPMs, feed motors, auto margin

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -62,15 +62,18 @@ public final class Constants {
     }
 
     public static class MotorControllers {
-        public static final int ID_LEFT_FRONT = 30; //30; //40; //10
+        public static final int ID_LEFT_FRONT = 30; //10
         public static final int ID_LEFT_REAR = 44; //11
         public static final int ID_RIGHT_FRONT = 43; //15
         public static final int ID_RIGHT_REAR = 45; //16
 
-        public static final int BOTTOM_SHOOTER = 36; //1
-        public static final int TOP_SHOOTER = 33; //7
-
         public static final int INTAKE = 39;
+
+        public static final int FIRST_FEEDER = 46;
+        public static final int SECOND_FEEDER = 47;
+
+        public static final int BOTTOM_SHOOTER = 36;
+        public static final int TOP_SHOOTER = 33;
 
         public static final int ID_MAST = 38;
         public static final int ID_ARM = 37;
@@ -102,12 +105,12 @@ public final class Constants {
         public static final double GEAR_RATIO = 8.71 * 0.75;
 
         public static final double REV_TO_IN_K = CIRCUMFERENCE / GEAR_RATIO;
-        public static final double IN_TO_REV_K = GEAR_RATIO / CIRCUMFERENCE; //multiply circumference by 0.75
+        public static final double IN_TO_REV_K = GEAR_RATIO / CIRCUMFERENCE;
 
         public static final boolean IS_DEADZONE = true;
 
         // PID
-        public static double kP = 0.01; //0.01;
+        public static double kP = 0.01;
         public static double turnkP = 0.016;
         public static double kI = 0;
         public static double kD = 0;
@@ -118,45 +121,32 @@ public final class Constants {
         public static final double DISTANCE = 44;
         // one degree is 0.239 inches *if the relationship is linear*
         public static final double TURN_180 = 43;
-        public static final double TURN_135 = 25;
+        public static final double TURN_135 = 25.7;
         public static final double TURN_90 = 21.5;
-        public static final double TURN_70 = 15;
+        public static final double TURN_70 = 16;
         public static final double TURN_15 = 6.5; // need to test this angle; should be able to head toward loading station ball during position 2 auto
         public static final double TURN_18 = 4.5;
         // distances
+        public static final double AUTO_MARGIN = 0;
         public static final double HUB_TO_BALL = 95;
         public static final double TARMAC_TO_BALL = 55;
         public static final double TARMAC_TO_BALL_SHORT = 47; // short versions are for position 1 where the wall is close to ball
-        public static final double BALL_TO_LINE = 22;
-        public static final double BALL_TO_LINE_SHORT = 16; // short versions are for position 1 where the wall is close to ball
+        public static final double BALL_TO_LINE = 23 - AUTO_MARGIN;
+        public static final double BALL_TO_LINE_SHORT = 16 - AUTO_MARGIN; // short versions are for position 1 where the wall is close to ball
         public static final double TARMAC_TO_LINE = 20;
-        public static final double TARMAC_TO_LOADING = 166;
-	    
-	    public static class limelight {
-        // public static final double CATAPULTLEGNTH = ?;
-        // public static final double CATAPULTANGLE = ?;
-        // public static final double SPRINGCONSTANT = ?;   
-        // public static final double CAMHEIGHT = ?;
-        // public static final double CAMANGLE = ?;
-        // public static final int HUBHEIGHT = 104;
-        // public static final double TARMACDISTANCE = 84.75;
-        // public static final int TARGETDIAMETER = 48;
-        // public static final double TOTALANGLE = Math.tan((HUBHEIGHT-CAMHEIGHT) / TARMACDISTANCE);
-        // public static final double CAMANGLECOMPLIMENT = TOTALANGLE - CAMANGLE;
-        // public static final double TARGETAREA = Math.PI * TARGETDIAMETER;
-        // public static final double TRUEDISTANCE = (HUBHEIGHT-CAMHEIGHT)/(Math.tan(CAMANGLE-CAMANGLECOMPLIMENT)); 
-	    }
+        public static final double TARMAC_TO_LOADING = 156;
+	  
     }
     public static class ShooterConstants {
         public static final double kPBot = 0.0002;
         public static final double kIBot = 0.00000001;
         public static final double kDBot = 0.0565;
-        public static final double kFFBot = 0.00021; //0.000215;//.00022
+        public static final double kFFBot = 0.00021;
 
         public static final double kPTop = 0.0001;
         public static final double kITop = 0.00000001;
         public static final double kDTop = 0.0565;
-        public static final double kFFTop = 0.00018; //0.000215;//.00022
+        public static final double kFFTop = 0.00018;
 
         // speeds for top and bottom rollers
         // gear ratio is 18:32
@@ -165,30 +155,55 @@ public final class Constants {
         public static final double TOP_SPEED = (2000 * 18) / 32; 
         public static final double BOT_SPEED = (2000 * 18) / 32; 
 
-        public static final double HIGH_HUB_TOP = (6000 * 18) / 32; //-1800; //2790; //3234; //5750;
-        public static final double HIGH_HUB_BOT = (2200 * 18) / 32; //2220; //1600; //1530; //2720;
+        public static final double HIGH_HUB_TOP = (6000 * 18) / 32;
+        public static final double HIGH_HUB_BOT = (2200 * 18) / 32;
 
         public static final double TOP_BASKETBALL = (6000 * 18) / 32;
         public static final double BOT_BASKETBALL = (3850 * 18) / 32;
 
-        public static final double LOW_HUB_TOP = (3400 * 18) / 32; //3400;
-        public static final double LOW_HUB_BOT = (1700 * 18) / 32; //1700;
+        public static final double LOW_HUB_TOP = (3400 * 18) / 32;
+        public static final double LOW_HUB_BOT = (1700 * 18) / 32;
 
         // **** good tarmac shot: rollers should be at top:3650 and bot: 3466 ****
-        public static final double TARMAC_TOP = (3650 * 18) / 32; //1237; // wheel speed = 2200;
-        public static final double TARMAC_BOT = (3350 * 18) / 32; //2261; // wheel speed = 4020;
+        public static final double TARMAC_TOP = (5000 * 18) / 32; //started with 5000
+        public static final double TARMAC_BOT = (2500 * 32) / 18; // 2500
+        // with waterbury pulley ratio
+        // public static final double TARMAC_TOP = (3650 * 18) / 32; //1237; // wheel speed = 2200; //3650
+        // public static final double TARMAC_BOT = (3350 * 18) / 32; //2261; // wheel speed = 4020;
 
-// ***increase top and decrease bottom for more height and less depth
-        public static final double LAUNCH_PAD_TOP = (6000 * 18) / 32;
-        public static final double LAUNCH_PAD_BOT = (3000 * 18) / 32;
+        public static final double TARMAC_ATOP = (3350 * 18) / 32; //1237; // wheel speed = 2200; //3650
+        public static final double TARMAC_ABOT = (3325 * 18) / 32; //2261; // wheel speed = 4020;
+
+        // 3650, 3075
+        // 3350, 3325
+        // 6000, 2500
+
+        // increase bottom for more loft, decrease top to make shorter & less backspin
+        public static final double TARMAC_TOP_XSHORT = (3400 * 18) / 32; //1237; // wheel speed = 2200; //3650
+        public static final double TARMAC_BOT_XSHORT = (3600 * 18) / 32; //2261; // wheel speed = 4020;
+
+        public static final double TARMAC_TOP_SHORT = (3500 * 18) / 32; //1237; // wheel speed = 2200; //3650
+        public static final double TARMAC_BOT_SHORT = (3600 * 18) / 32; //2261; // wheel speed = 4020;
+
+        public static final double TARMAC_TOP_LONG = (3670 * 18) / 32; //1237; // wheel speed = 2200; //3650
+        public static final double TARMAC_BOT_LONG = (3520 * 18) / 32; //2261; // wheel speed = 4020;
+
+        // ***increase top and decrease bottom for more height and less depth
+        public static final double LAUNCH_PAD_TOP = (7450 * 18) / 32; //6050
+        public static final double LAUNCH_PAD_BOT = (3450 * 18) / 32; //2850
+        // 7510, 3500 was too long
     }
     public static class IntakeConstants {
-        public static final int DIO_INTAKE_COUNTER = 5;
+        public static final int DIO_INTAKE_EYE = 5;
+        public static final int DIO_FEEDER_EYE = 2;
 
-        public static final double FORWARD_SPEED = 0.8; // 0.7 works well with purple fabric
+        public static final double FORWARD_SPEED = 0.85;
         // 0.6 voltage = 930 RPM
         // 0.8 voltage = 1280 RPM
         public static final double REVERSE_SPEED = -0.5;
+
+        public static final double FIRST_FEED_SPEED = 0.3;
+        public static final double SECOND_FEED_SPEED = 0.3;
     }
     public static class ColorSensorConstants {
         public static final int DIST = 300;
